7d380cc8654095da5baff881167ae03fe72558e9
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2026-09-26 12:34:09 +0200

    fix: correct ApiResource attribute by moving 'openapi' flag to the correct position

--- src/ApiResource/AccountVerification.php
+++ src/ApiResource/AccountVerification.php
@@ -8,7 +8,7 @@ use Nurschool\State\VerificationProcessor;
 #[ApiResource(operations: [new Post(
     uriTemplate: '/account-verifications', formats: ['json' => ['application/json']],
     read: false, deserialize: false, validate: false, output: false,
-    processor: VerificationProcessor::class, name: 'api_verification',
+    openapi: false, processor: VerificationProcessor::class, name: 'api_verification',
 )])]
 final class AccountVerification
 {
